79067e616294029632daf3d38a2534629b449e66
David Stalnaker <david.stalnaker@gmail.com>
2015-04-09 00:33:16 -0400

    Begin implementation of add - generate the hash.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate clap;
-use clap::{App, SubCommand};
+extern crate sha1;
+
+use clap::{App, Arg, SubCommand};
 use std::{io, fs};
-use std::io::Write;
+use std::io::{Read, Write};
 use std::fs::File;
 use std::path::Path;
 
@@ -9,12 +11,28 @@ fn main() {
     let matches = App::new("Grit")
         .subcommand(SubCommand::new("init")
                     .about("Initializes the repo"))
+        .subcommand(SubCommand::new("add")
+                    .about("Adds a file")
+                    .arg(Arg::new("file")
+                         .help("File to add")
+                         .index(1)
+                         .multiple(true)
+                         .required(true)))
         .get_matches();
 
     if let Some(..) = matches.subcommand_matches("init") {
         match init() {
             Ok(()) => println!("Initialized."),
             Err(..) => println!("Error: already initialized.")
+        }
+    }
+
+    if let Some(submatches) = matches.subcommand_matches("add") {
+        for f in submatches.values_of("file").unwrap() {
+            match add(f) {
+                Ok(()) => println!("Initialized."),
+                Err(e) => println!("Error: {}", e)
+            }
         }
     }
 }
@@ -29,3 +47,22 @@ fn init() -> io::Result<()> {
     let mut head = try!(File::create(grit_dir.join("HEAD")));
     head.write_all("ref: refs/heads/master".as_bytes())
 }
+
+fn add(to_add: &str) -> io::Result<()> {
+    let path = Path::new(to_add);
+    let mut f = try!(File::open(path));
+    let mut bytes = Vec::new();
+    try!(f.read_to_end(&mut bytes));
+
+    let mut sha = sha1::Sha1::new();
+    sha.update(&bytes);
+    let hash = sha.hexdigest();
+
+    let objects = Path::new(".grit/objects");
+    let blob_dir = objects.join(&hash[..2]);
+    let blob = blob_dir.join(&hash[2..]);
+    println!("{}: {}", to_add, blob.to_str().unwrap());
+
+    return Ok(());
+}
+
