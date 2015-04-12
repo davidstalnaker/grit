bbb8138c7c20c67fa44ac01d4f18e4a67b261101
Alexander Dean <dstar@slackless.com>
2015-04-11 21:39:05 -0700

    Improves grit add for relative paths.

--- src/add.rs
+++ src/add.rs
@@ -5,16 +5,47 @@ use std::collections::HashMap;
 use std::io::{BufReader, BufRead, Read, Write};
 use std::fs::File;
 use std::path::{Path, PathBuf};
+use std::env;
 
-pub fn add_all(to_add: &Vec<&str>) -> io::Result<()> {
-    let mut index = try!(Index::new());
-    for filename in to_add {
-        match write_blob(filename) {
-            Ok(hash) => index.update(filename, &hash),
+pub fn add_all(root_dir: &PathBuf, to_add: &Vec<&str>) -> io::Result<()> {
+    let mut index = try!(Index::new(&root_dir));
+    let filepaths = build_file_list( &to_add );
+    for filename in filepaths {
+        match write_blob(&filename, &root_dir) {
+            Ok(hash) => {
+                let relative_path = relative_from( &filename, root_dir ).unwrap();
+                index.update( &relative_path.to_str().unwrap(), &hash)
+            },
             Err(e) => return Err(e)
         }
     }
     index.write()
+}
+
+fn relative_from<'a, P: Sized + AsRef<Path>>( abs: &'a P, root: &'a P ) -> Option<&'a Path> {
+    // See Path.relative_from(*) which is currently unstable.
+    iter_after(abs.as_ref().components(), root.as_ref().components()).map(|c| c.as_path())
+}
+fn iter_after<A, I, J>( mut iter: I, mut prefix: J ) -> Option<I> where
+    I: Iterator<Item=A> + Clone, J: Iterator<Item=A>, A: PartialEq 
+{
+    loop {
+        let mut iter_next = iter.clone();
+        match( iter_next.next(), prefix.next() ){
+            (Some(x), Some(y)) => {
+                if x != y { return None }
+            },
+            (Some(_), None) => return Some(iter),
+            (None, None) => return Some(iter),
+            (None, Some(_)) => return None,
+        }
+        iter = iter_next;
+    }
+}
+
+fn build_file_list(paths: &Vec<&str> ) -> Vec<PathBuf> {
+    let cur_dir = env::current_dir().unwrap();
+    paths.iter().map(|path| cur_dir.join(path)).collect::<Vec<_>>() 
 }
 
 pub struct Index {
@@ -23,9 +54,9 @@ pub struct Index {
 }
 
 impl Index {
-    pub fn new() -> io::Result<Index> {
+    pub fn new(root_dir: &PathBuf) -> io::Result<Index> {
         let mut index = Index {
-            path: Box::new(Path::new(".grit").join("index")),
+            path: Box::new(root_dir.join(".grit").join("index")),
             hashes: HashMap::new()
         };
         if !path_exists(&*index.path) {
@@ -47,10 +78,9 @@ impl Index {
     pub fn update(&mut self, path: &str, hash: &str) {
         self.hashes.insert(path.to_string(), hash.to_string());
     }
-
-    pub fn print(&self) {
+pub fn print(&self) {
         for (ref hash, ref path) in self.hashes.iter() {
-            println!("{}, {}", hash, path);
+            println!("{} {}", hash, path);
         }
     }
 
@@ -67,8 +97,7 @@ fn path_exists(path : &PathBuf) -> bool {
     fs::metadata(path).is_ok()
 }
 
-pub fn write_blob(to_add: &str) -> io::Result<String> {
-    let path = Path::new(to_add);
+pub fn write_blob(path: &PathBuf, root_dir: &PathBuf) -> io::Result<String> {
     let mut f = try!(File::open(path));
     let mut bytes = Vec::new();
     try!(f.read_to_end(&mut bytes));
@@ -77,7 +106,7 @@ pub fn write_blob(to_add: &str) -> io::Result<String> {
     sha.update(&bytes);
     let hash = sha.hexdigest();
 
-    let objects = Path::new(".grit/objects");
+    let objects = root_dir.join(".grit").join("objects");
     let blob_dir = objects.join(&hash[..2]);
     if !path_exists(&blob_dir) {
         try!(fs::create_dir(&blob_dir));
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate clap;
 extern crate grit;
 
 use clap::{App, Arg, SubCommand};
+use std::fs;
+use std::path::{PathBuf};
+use std::env;
 
 fn main() {
     let matches = App::new("Grit")
@@ -23,12 +26,37 @@ fn main() {
         }
     }
 
-    if let Some(submatches) = matches.subcommand_matches("add") {
-        match grit::add_all(&submatches.values_of("file").unwrap()) {
-            Ok(()) => (),
-            Err(e) => println!("Error: {}", e)
+    match find_root_dir() {
+        Ok(root_dir) => {
+            if let Some(submatches) = matches.subcommand_matches("add") {
+                match grit::add_all(&root_dir, &submatches.values_of("file").unwrap()) {
+                    Ok(()) => (),
+                    Err(e) => println!("Error: {}", e)
+                }
+            }
+        },
+        Err(msg) => { println!("{}", msg); }
+    }
+    
+
+}
+
+fn find_root_dir() -> Result<PathBuf,String> {
+    let mut cur_dir = env::current_dir().unwrap();
+    loop {
+        if is_grit_dir( &cur_dir ) {
+            return Ok( cur_dir )
+        }
+        if !cur_dir.pop() {
+            return Err("No grit directory found.".to_string());
         }
     }
 }
 
+fn path_exists(path : &PathBuf) -> bool {
+    fs::metadata(path).is_ok()
+}
 
+fn is_grit_dir( path : &PathBuf ) -> bool {
+    path_exists( &path.join(".grit") )
+}
